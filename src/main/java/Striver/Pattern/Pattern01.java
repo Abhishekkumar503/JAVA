e317d13d6d4ef6b26695959bc45310993d20fbae
Abhishek Kumar <ak204479@gmail.com>
2025-02-25 23:38:05 +0530

    Pattern01

---
+++ src/main/java/Striver/Pattern/Pattern01.java
@@ -0,0 +1,28 @@
+package Striver.Pattern;
+
+public class Pattern01 {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 0; i <n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/*
+QUESTION  :  https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
+
+OUTPUT :-
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+
+ */
