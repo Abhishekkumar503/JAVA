1406be9281df57d114e84f8fb98c922e8e8d5ee2
Abhishek Kumar <ak204479@gmail.com>
2025-02-28 00:13:23 +0530

    Fixed  Pattern10

--- src/main/java/Striver/Pattern/Pattern10.java
+++ src/main/java/Striver/Pattern/Pattern10.java
@@ -3,13 +3,15 @@ package Striver.Pattern;
 public class Pattern10 {
     public static void main(String[] args) {
         int n=5;
-        for(int rows = 0; rows < 2*n+1; rows++  )
+        for(int rows = 1; rows <= 2*n+1; rows++  )
         {
-            for (int stars = 0; stars < rows+1; stars++) {
+            int end=0;
+            if(rows > n)
+                end=2*n-rows;
+            else
+                end=rows;
+            for (int stars = 0; stars < end; stars++) {
                 System.out.print("*");
-            }
-            for (int spaces = 0; spaces < n-rows-1; spaces++) {
-                System.out.print(" ");
             }
             System.out.println();
         }
