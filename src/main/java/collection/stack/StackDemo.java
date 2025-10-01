156bfede59dc636055b762e4dbee0e16f37e255a
Abhishekkumar503 <ak204479@gmail.com>
2025-10-01 22:44:28 +0530

    Stack

---
+++ src/main/java/collection/stack/StackDemo.java
@@ -0,0 +1,21 @@
+package collection.stack;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Stack<String> stack = new Stack<>();
+		stack.push("ABHISHEK");
+		stack.add("Sandesh");
+		
+		System.out.println(stack);
+		
+		System.out.println(stack.empty());
+		
+		
+		}
+
+}
